73a17a477661c4698049d6ec9a21596d3a2cc017
stu2001321077-code <stu2001321077@uni-plovdiv.bg>
2026-09-25 12:17:55 +0300

    четно/нечетно с вход от конзолата

--- main.go
+++ main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+func proveriChislo(n int) (string, error) {
+    if n == 0 {
+        return "", fmt.Errorf("нулата не е нито четна, нито нечетна")
+    }
+    if n%2 == 0 {
+        return "четно", nil
+    }
+    if n%2 == 1 {
+        return "нечетно", nil
+    }
+    return "", fmt.Errorf("числото %d е отрицателно", n)
+}
+
 func main() {
-	fmt.Println("Здравей, Go!")
+    var n int
+    fmt.Print("Въведи число: ")
+    _, err := fmt.Scan(&n)
+    if err != nil {
+        fmt.Println("Това не е число")
+        return
+    }
+
+    rez, err := proveriChislo(n)
+    if err != nil {
+        fmt.Println("Грешка:", err)
+        return
+    }
+    fmt.Println(n, "е", rez)
 }
